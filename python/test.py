8a68a4835a3263b0128439cac5f5a27283586b6b
whale <2966365639@qq.com>
2023-08-29 20:50:41 +0800

    finsih：循迹模块的py文件完成

--- python/test.py
+++ python/test.py
@@ -10,40 +10,39 @@ from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 
 
-def callback():
-    simplegoal_pub = rospy.Publisher('/move_base_simple/goal',PoseStamped, queue_size=10)  # 发布节点名称: followingpath
-    rospy.init_node("goalpub", anonymous=True)                   # 初始化节点 名称:pubpath
-    rate = rospy.Rate(1)                                                        # 设置ros发布频率，单
-    poseList = []
-    with open(pathfile,'r') as lines:
-        for line in lines:
-            rows = line.strip('\n').split('|')
-            posestamped = PoseStamped()                                                 # 初始化消息类型
-            posestamped.header.frame_id = "map"
-            posestamped.header.stamp = rospy.get_rostime()                                     # 获取ros当前时间
-            posestamped.pose.position.x = float(rows[0])
-            posestamped.pose.position.y = float(rows[1]) 
-            posestamped.pose.position.z = float(rows[2])
-            posestamped.pose.orientation.x = float(rows[3]) 
-            posestamped.pose.orientation.y = float(rows[4])
-            posestamped.pose.orientation.z = float(rows[5])
-            posestamped.pose.orientation.w = float(rows[6])
-            poseList.append(posestamped)
-    
-    while not rospy.is_shutdown():
-        simplegoal_pub.publish(poseList[0])
-        #poseList.pop(0)
-        rate.sleep()
+class Hunter_core():
+    def __init__(self):
+        rospy.init_node("hunter_core", anonymous=True)
+        #self.wps_buf = []
+  
+        rospy.Subscriber("/odom", Odometry, self.wpsCallback, queue_size=10)
+        rospy.spin()            
+            
+    def wpsCallback(self, msgs):
+        p1 = np.zeros(7)
+        np.set_printoptions(suppress=True)  # 取消科学记数法
+        np.set_printoptions(threshold=np.inf)  # 数据完整打印
+        p1[0] = msgs.pose.pose.position.x
+        p1[1] = msgs.pose.pose.position.y
+        p1[2] = msgs.pose.pose.position.z
+        p1[3] = msgs.pose.pose.orientation.x
+        p1[4] = msgs.pose.pose.orientation.y
+        p1[5] = msgs.pose.pose.orientation.z
+        p1[6] = msgs.pose.pose.orientation.w
+        
+        print(">>>>" + str(p1))
+        for p in p1:
+            f1.write(str(p) + '|')
+        f1.write("\n")
+        print("save way point success!!!")
+        #print(np.array(self.wps_buf))
+        #print("p:"+str(p1))
+
 
 
 if __name__ == '__main__':
-    pathfile = "/root/catkin_ws/python/goal.txt"
-    try:
-        callback()
-    except rospy.ROSInterruptException:
-        pass
-                                
-
-
-
-
+    filename = './wps.txt'
+    f1 = open(filename,'w')
+    f1.write("position.x|position.y|position.z|orientation.x|orientation.y|orientation.z|orientation.w" + "\n")
+    hunter_core = Hunter_core()
+    f1.close()
